ae19903fe5a42c5c19aa956c3baed52f53f65638
Hariharan P R <prhariharan2005@gmail.com>
2024-10-02 18:58:41 +0530

    Notebook folder added

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from typing import List
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    this function will return the list of requirements
     '''
-
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements ]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
+    
     return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Hariharan',
-    author_email='prhariharan2005@gmail.com',
-    package= find_packages(),
-    install_requires=get_requirements('requirements.txt')
-    )
+name='mlproject',
+version='0.0.1',
+author='Hariharan',
+author_email='prhariharan2005@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
